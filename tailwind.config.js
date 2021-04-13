121580338b01e8fea9895bc968cf42fc46961ff4
Simon Jensen <mail@simonjensen.net>
2021-04-13 22:28:58 +0200

    Use correct font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'serif': ['Roboto Slab Light', 'serif']
+      }
+    },
   },
   variants: {
     extend: {},
