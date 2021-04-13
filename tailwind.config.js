53cd51f5fe1b08b8402da0b7434d832eaf811d80
Simon Jensen <mail@simonjensen.net>
2021-04-13 21:46:57 +0200

    Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './public/index.html',
+    './src/**/*.{js,vue}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
